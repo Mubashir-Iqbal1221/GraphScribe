c3e66c9a9b63f9ab9f087052ed3cb66ebbf23651
Mubashir Iqbal <Mubashiriqbal1221@gmail.com>
2024-09-05 17:37:18 +0500

    add: exception handling

--- src/image_to_text.py
+++ src/image_to_text.py
@@ -12,9 +12,14 @@ class paddle_ocr_wrapper:
     Load PaddleOcr model and provide funtion "paddle_ocr" to extract text from image
     """
     def __init__(self):
-        self.config = load_config()
-        self.ocr = PaddleOCR(use_angle_cls = self.config["ocr"]["use_angle_cls"], lang=self.config["ocr"]["lang"])
-    
+        try:
+            self.config = load_config()
+            self.ocr = PaddleOCR(use_angle_cls = self.config["ocr"]["use_angle_cls"], 
+                                 lang=self.config["ocr"]["lang"])
+
+        except Exception as e:
+            raise RuntimeError(f"Failed to initialize PaddleOCR: {e}")
+            
     def paddle_ocr(self,preprocessed_image:np.ndarray)-> list:
         """
         Extract text from a preprocessed image using PaddleOCR.
@@ -37,15 +42,19 @@ class paddle_ocr_wrapper:
                 - predicted_text (str): The text recognized by PaddleOCR.
                 - confidence_score (float): The confidence score of the OCR prediction.
         """
-        image = preprocessed_image.copy()
-        if len(image.shape) == 3 and image.shape[2] == 3:
-            image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        else:
-            image_rgb = image
-        # Perform OCR
-        result = self.ocr.ocr(image_rgb, cls=True)
+        
+        try:
+            image = preprocessed_image.copy()
+            if len(image.shape) == 3 and image.shape[2] == 3:
+                image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            else:
+                image_rgb = image
+            # Perform OCR
+            result = self.ocr.ocr(image_rgb, cls=True)
 
-        return result[0]
+            return result[0]
+        except Exception as e:
+            raise RuntimeError(f"Error during OCR processing: {e}")
     
 def display_annotated_image(ocr_results:list,image_path:str)-> None:
     """
@@ -63,41 +72,44 @@ def display_annotated_image(ocr_results:list,image_path:str)-> None:
         None: This function displays the annotated image and does not return any value.
 
     """
-    image = Image.open(image_path)
-    config = load_config()
-    # print(f"figsize={tuple(config['display']['figure_size'])}")
+    try:
+        image = Image.open(image_path)
+        config = load_config()
+        # print(f"figsize={tuple(config['display']['figure_size'])}")
 
-    # Create a figure and axis to plot on
-    fig, ax = plt.subplots(1,figsize=(config["display"]["figure_width"],
-                                      config["display"]["figure_height"]))
-    ax.imshow(image)
+        # Create a figure and axis to plot on
+        fig, ax = plt.subplots(1,figsize=(config["display"]["figure_width"],
+                                        config["display"]["figure_height"]))
+        ax.imshow(image)
 
-    for result in ocr_results:
-        p1 = result[0][0]
-        p2 = result[0][1]
-        p3 = result[0][2]
-        p4 = result[0][3]
-        word = result[1][0]
-        score = result[1][1]
+        for result in ocr_results:
+            p1 = result[0][0]
+            p2 = result[0][1]
+            p3 = result[0][2]
+            p4 = result[0][3]
+            word = result[1][0]
+            score = result[1][1]
 
-        width = p2[0] - p1[0]
-        height = p3[1] - p1[1]
-        x1 = p1[0]
-        y1 = p1[1]
+            width = p2[0] - p1[0]
+            height = p3[1] - p1[1]
+            x1 = p1[0]
+            y1 = p1[1]
 
-        # Create a rectangle patch and add it to the plot
-        rect = patches.Rectangle((x1, y1), width, height, linewidth=config["display"]["linewidth"],
-                                 edgecolor=config["display"]["edgecolor"], facecolor=config["display"]["facecolor"])
-        ax.add_patch(rect)
+            # Create a rectangle patch and add it to the plot
+            rect = patches.Rectangle((x1, y1), width, height, linewidth=config["display"]["linewidth"],
+                                    edgecolor=config["display"]["edgecolor"], facecolor=config["display"]["facecolor"])
+            ax.add_patch(rect)
 
-        # Annotate the word with its confidence, placing it slightly above the top-left corner of the box
-        annotation = f"{word} ({score:.2f})"
-        ax.text(x1, y1 - config["display"]["annotation_offset"], annotation, 
-                color=config["display"]["annotation_color"], fontsize=config["display"]["annotation_fontsize"])
+            # Annotate the word with its confidence, placing it slightly above the top-left corner of the box
+            annotation = f"{word} ({score:.2f})"
+            ax.text(x1, y1 - config["display"]["annotation_offset"], annotation, 
+                    color=config["display"]["annotation_color"], fontsize=config["display"]["annotation_fontsize"])
 
-    # Hide axes
-    plt.axis('off')
-    plt.show()
+        # Hide axes
+        plt.axis('off')
+        plt.show()
+    except Exception as e:
+        raise RuntimeError(f"Error while displaying annotated image : {e}")
 
 
 
--- src/util.py
+++ src/util.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
 import yaml
+import os
 
 def preprocess_image(image_path:str) -> np.ndarray:
     """
@@ -13,20 +14,37 @@ def preprocess_image(image_path:str) -> np.ndarray:
     Returns:
         np.ndarray : Preprocessed image in BGR form
     """
-    image = cv2.imread(image_path) # Read the image
-    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) # Convert to grayscale
-    kernel = np.ones((2, 2), np.uint8) # kernel for dilation
-    dilated_image = cv2.dilate(gray_image, kernel, iterations=1) #Appling dilation to remove noise form words
-    kernel = np.ones((5, 5), np.uint8) # kernel for erosion
-    eroded_image = cv2.erode(dilated_image, kernel, iterations=1) # applying erosion to fill gaps in text boundaries
-    preprocessed_image = cv2.cvtColor(eroded_image, cv2.COLOR_GRAY2BGR) #Convert the processed image back to a 3-channel image
+    try:
+        image = cv2.imread(image_path) # Read the image
+        if image is None:
+            raise FileNotFoundError(f"Image not found or unable to load: {image_path}")
 
-    return preprocessed_image
+        gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) # Convert to grayscale
+        kernel = np.ones((2, 2), np.uint8) # kernel for dilation
+        dilated_image = cv2.dilate(gray_image, kernel, iterations=1) #Appling dilation to remove noise form words
+        kernel = np.ones((5, 5), np.uint8) # kernel for erosion
+        eroded_image = cv2.erode(dilated_image, kernel, iterations=1) # applying erosion to fill gaps in text boundaries
+        preprocessed_image = cv2.cvtColor(eroded_image, cv2.COLOR_GRAY2BGR) #Convert the processed image back to a 3-channel image
+
+        return preprocessed_image
+    except Exception as e:
+        raise RuntimeError(f"Error raised during Image processing {e}")
 
 
 def load_config() -> dict:
     """
     Loads the YAML configuration file and returns it as a Python dictionary.
     """
-    with open("/home/mubashir/onboarding_project/config.yaml", "r") as config_file:
-        return yaml.safe_load(config_file)
+    config_path = "/home/mubashir/onboarding_project/config.yaml"
+    
+    # Check if the file exists
+    if not os.path.exists(config_path):
+        raise FileNotFoundError(f"Configuration file not found: {config_path}")
+    
+    try:
+        with open(config_path, "r") as config_file:
+            return yaml.safe_load(config_file)
+    except yaml.YAMLError as e:
+        raise yaml.YAMLError(f"Error parsing YAML configuration file: {e}")
+    except Exception as e:
+        raise Exception(f"Error loading configuration file: {e}")
