988cf93ca4abbfec3355e226e070d92d41142ebb
Mubashir Iqbal <Mubashiriqbal1221@gmail.com>
2024-09-25 16:19:29 +0500

    remove: unnecessary files

--- start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Start the FastAPI app in the background
-uvicorn app:app --host 0.0.0.0 --port 8000 --reload &
-
-# Start the Streamlit app
-streamlit run demo.py --server.port 8501 --server.address 0.0.0.0
